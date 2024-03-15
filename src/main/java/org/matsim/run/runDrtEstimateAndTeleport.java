01f8205637fe7f17d0dd50bf0240e179dd1d9a9f
Chengqi Lu <43133404+luchengqi7@users.noreply.github.com>
2024-03-15 13:30:01 +0100

    Further setup
    Add an example scenario and run script

--- src/main/java/org/matsim/run/runDrtEstimateAndTeleport.java
+++ src/main/java/org/matsim/run/runDrtEstimateAndTeleport.java
@@ -2,7 +2,9 @@ package org.matsim.run;
 
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.application.MATSimApplication;
+import org.matsim.contrib.drt.estimator.DrtEstimator;
 import org.matsim.contrib.drt.estimator.DrtEstimatorModule;
+import org.matsim.contrib.drt.estimator.impl.DetourBasedDrtEstimator;
 import org.matsim.contrib.drt.estimator.impl.PessimisticDrtEstimator;
 import org.matsim.contrib.drt.routing.DrtRoute;
 import org.matsim.contrib.drt.routing.DrtRouteFactory;
@@ -10,6 +12,7 @@ import org.matsim.contrib.drt.run.DrtConfigGroup;
 import org.matsim.contrib.drt.run.DrtConfigs;
 import org.matsim.contrib.drt.run.MultiModeDrtConfigGroup;
 import org.matsim.contrib.drt.run.MultiModeDrtModule;
+import org.matsim.contrib.dvrp.run.AbstractDvrpModeModule;
 import org.matsim.contrib.dvrp.run.DvrpConfigGroup;
 import org.matsim.contrib.dvrp.run.DvrpModule;
 import org.matsim.contrib.dvrp.run.DvrpQSimComponents;
@@ -21,20 +24,23 @@ import picocli.CommandLine;
 
 import javax.annotation.Nullable;
 
-@CommandLine.Command(header = ":: Run insertion strategy ::", version = runDrtScenario.VERSION)
-public class runDrtScenario extends MATSimApplication {
+@CommandLine.Command(header = ":: Run estimate and teleport ::", version = runDrtEstimateAndTeleport.VERSION)
+public class runDrtEstimateAndTeleport extends MATSimApplication {
     static final String VERSION = "1.0";
 
     public static void main(String[] args) {
-        MATSimApplication.run(runDrtScenario.class, args);
+        MATSimApplication.run(runDrtEstimateAndTeleport.class, args);
     }
 
     @Nullable
     @Override
     protected Config prepareConfig(Config config) {
-        MultiModeDrtConfigGroup multiModeDrtConfig = ConfigUtils.addOrGetModule(config, MultiModeDrtConfigGroup.class);
         ConfigUtils.addOrGetModule(config, DvrpConfigGroup.class);
+        MultiModeDrtConfigGroup multiModeDrtConfig = ConfigUtils.addOrGetModule(config, MultiModeDrtConfigGroup.class);
         DrtConfigs.adjustMultiModeDrtConfig(multiModeDrtConfig, config.scoring(), config.routing());
+        for (DrtConfigGroup drtCfg: multiModeDrtConfig.getModalElements()) {
+            drtCfg.simulationType = DrtConfigGroup.SimulationType.estimateAndTeleport;
+        }
         return config;
     }
 
@@ -56,7 +62,6 @@ public class runDrtScenario extends MATSimApplication {
 
         MultiModeDrtConfigGroup multiModeDrtConfigGroup = MultiModeDrtConfigGroup.get(config);
         for (DrtConfigGroup drtCfg : multiModeDrtConfigGroup.getModalElements()) {
-            drtCfg.simulationType = DrtConfigGroup.SimulationType.estimateAndTeleport;
             // add the DRT estimator binding
             controler.addOverridingModule(new AbstractModule() {
                 @Override
--- src/test/java/org/matsim/run/DummyTest.java
+++ src/test/java/org/matsim/run/DummyTest.java
@@ -1,16 +1,12 @@
 package org.matsim.run;
 
 import org.junit.jupiter.api.Test;
-import org.matsim.testcases.MatsimTestUtils;
 
 public class DummyTest {
-    public MatsimTestUtils utils = new MatsimTestUtils();
-
     @Test
     public final void testRunScript() {
         //TODO here is a dummy test. Add a real test later
         System.out.println("Starting dummy test");
-        System.out.println("The output directory is " + utils.getOutputDirectory());
         double x = 1;
         double y = 2;
         double z = x + y;
